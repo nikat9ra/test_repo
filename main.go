7ceb2043ee76b6021ca36c8a15fbc07a866e0398
Roman Kanaykin <r.kanaykin@smartway.today>
2022-01-22 12:17:06 +0300

    second commit

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	printMessage("Vasya")
+	printMessage("Petya")
+	printMessage("Ivan")
+}
+func printMessage(message string) {
+	fmt.Println(message)
 }
