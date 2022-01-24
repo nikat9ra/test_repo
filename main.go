08cb1a091857fa0bb85d62bcca53e62d96ce0692
Roman Kanaykin <r.kanaykin@smartway.today>
2022-01-24 12:24:21 +0300

    four commit

--- main.go
+++ main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func main() {
-	message := "Хеллоу ворлд"
-	fmt.Println(&message)
-	fmt.Println(message)
-	changeMessage(&message)
+	// messages := []string{"1", "2", "3"}
+	messages := make([]string, 5, 15)
+	messages[0] = "1"
+	// printMessage(messages)
 
-	fmt.Println(message)
-
+	fmt.Println(messages)
 }
+func printMessage(messages []string) error {
 
-func changeMessage(message *string) {
+	if len(messages) == 0 {
+		return errors.New("empty array")
+	}
+	messages[1] = "5"
+	fmt.Println(messages)
 
-	*message += " от функции изменить"
+	return nil
 }
