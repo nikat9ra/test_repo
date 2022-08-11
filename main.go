8d39ee118585c40f917b89f3f90fa28e22899759
Roman Kanaykin <r.kanaykin@gmail.com>
2022-08-11 19:35:27 +0300

    test commit

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"rsc.io/quote"
+)
+
+const myUrl = "https://httpbin.org/"
 
 func main() {
 	matrix := make([][]int, 10)
@@ -16,4 +26,34 @@ func main() {
 		fmt.Println(matrix[x])
 	}
 
+	fmt.Println(quote.Go())
+	MakeRequest()
+}
+func MakeRequest() {
+
+	message := map[string]interface{}{
+		"hello": "world",
+		"life":  42,
+		"embedded": map[string]string{
+			"yes": "of course",
+		},
+	}
+
+	bytesRepresentation, err := json.Marshal(message)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	resp, err := http.Post(myUrl+"post", "application/json", bytes.NewBuffer(bytesRepresentation))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var result map[string]interface{}
+
+	json.NewDecoder(resp.Body).Decode(&result)
+
+	log.Println(result)
+	log.Println(result["data"])
+	//	fmt.Println(string(body))
 }
